a512ca080384dfa3250f79d64e1c2316070cdba2
GabrielSOliv <100054103+GabrielSOliv@users.noreply.github.com>
2023-02-21 21:29:09 -0300

    "Criando Arquivos"

---
+++ instagram/instagram/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  instagram
+//
+//  Created by Gabriel Oliveira on 21/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ instagram/instagram/instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by Gabriel Oliveira on 21/02/23.
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

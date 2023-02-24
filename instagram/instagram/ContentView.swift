3f8823a559efaa21469a13d073dd1a2d1bbd391e
GabrielSOliv <100054103+GabrielSOliv@users.noreply.github.com>
2023-02-23 21:21:05 -0300

    Criando TopBar

--- instagram/instagram/ContentView.swift
+++ instagram/instagram/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color("BackgroundColor").ignoresSafeArea()
+            VStack {
+                HStack{
+                   Image("Logo")
+                        .frame(width: 103, height: 29)
+                        .padding(.leading, 16)
+                        .padding(.top, 20)
+                        .padding(.trailing, 128)
+                    Spacer()
+                    Image("Upload")
+                         .frame(width: 24, height: 24)
+                         .padding(.trailing, 20)
+                         .padding(.top, 20)
+                     Spacer()
+                    Image("Like")
+                         .frame(width: 24, height: 24)
+                         .padding(.trailing, 20)
+                         .padding(.top, 20)
+                     Spacer()
+                    Image("Message")
+                         .frame(width: 24, height: 24)
+                         .padding(.trailing, 20)
+                         .padding(.top, 20)
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 

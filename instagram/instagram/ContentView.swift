6ffc74f7f0d0587590a13c807ebe9ff48e2bfad8
GabrielSOliv <100054103+GabrielSOliv@users.noreply.github.com>
2023-02-26 15:40:15 -0300

    Projeto Finalizado

--- instagram/instagram/ContentView.swift
+++ instagram/instagram/ContentView.swift
@@ -13,25 +13,22 @@ struct ContentView: View {
             Color("BackgroundColor").ignoresSafeArea()
             VStack {
                 Header()
-                ScrollView(.horizontal, showsIndicators: false){
-                    HStack(spacing: 15.0) {
-                        Story(image: "profile1", name: "Larissa Yam")
-                        Story(image: "profile2", name: "Vitor Gabriel")
-                        Story(image: "profile3", name: "Daniela Carla")
-                        Story(image: "profile4", name: "Lucas Moreira")
-                        Story(image: "profile5", name: "Dam")
-                        Story(image: "profile6", name: "Luna Lin")
-                        Story(image: "profile1", name: "Larissa Yam")
+                ScrollView(.vertical, showsIndicators: false){
+                    VStack{
+                        StorySection()
+                        PostSection(image: "profile1", name: "larissa12")
+                        PostSection(image: "profile2", name: "vitinBrabo")
+                        PostSection(image: "profile3", name: "daniz")
+                        PostSection(image: "profile4", name: "lucas15")
                     }
-                    .padding(.horizontal, 8)
                 }
-                .padding(.vertical, 10)
-                
-                Spacer()
+            }
+            TabBar()
+            
             }
         }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -53,7 +50,7 @@ struct Header: View{
             .padding(.horizontal, 15)
             .padding(.vertical, 3)
             
-        }
+        } .frame(width: UIScreen.main.bounds.width, height: 35)
     }
 }
 
@@ -78,6 +75,123 @@ struct Story: View {
             Text(name)
                 .font(.caption)
                 .foregroundColor(.white)
+        }
+    }
+}
+
+struct StorySection: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false){
+            HStack(spacing: 15.0) {
+                Story(image: "profile1", name: "Larissa Yam")
+                Story(image: "profile2", name: "Vitor Gabriel")
+                Story(image: "profile3", name: "Daniela Carla")
+                Story(image: "profile4", name: "Lucas Moreira")
+                Story(image: "profile5", name: "Dam")
+                Story(image: "profile6", name: "Luna Lin")
+                Story(image: "profile1", name: "Larissa Yam")
+            }
+            .padding(.horizontal, 8)
+        }
+        .padding(.vertical, 10)
+    }
+}
+
+struct PostSection: View {
+    var image: String = ""
+    var name: String = ""
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Image(image)
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .cornerRadius(50)
+                    .padding(.leading, 6)
+                Text(name)
+                    .foregroundColor(.white)
+                    .font(.caption)
+                Spacer()
+                Image("options")
+                    .padding()
+            }
+            Image(image)
+                .resizable()
+                .frame(width: 400, height: 375)
+                .padding(.bottom, -12)
+                .padding(.top, -12)
+            HStack(spacing: 12.0){
+                Image("Like")
+                    .padding(.leading)
+                Image("Comment")
+                Image("Share")
+                Spacer()
+                Image("Bookmark")
+                    .padding()
+            }
+            VStack{
+                HStack {
+                    Text(name)
+                        .foregroundColor(.white)
+                        .bold()
+                    Text("legend writer")
+                        .foregroundColor(.white)
+                    Spacer()
+                } .padding()
+                    .padding(.top, -30)
+                
+                HStack {
+                    Text("...")
+                        .foregroundColor(.white)
+                        .padding(.leading)
+                    Text("more")
+                        .foregroundColor(.gray)
+                        .padding(.leading, -4)
+                    Spacer()
+                } .padding(.top, -20)
+                HStack {
+                    Text("View all 2232 comments")
+                        .foregroundColor(.gray)
+                        .padding(.leading)
+                        .padding(.top, -8)
+                    Spacer()
+                }
+                HStack {
+                    Text("27 July ・")
+                        .foregroundColor(.gray)
+                    Text("See Translation")
+                        .foregroundColor(.white)
+                        .padding(.leading, -4)
+                    Spacer()
+                } .padding(.leading)
+                    .padding(.top, -9)
+            }
+        }
+    }
+}
+
+struct TabBar: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+                HStack(spacing: 0.0){
+                    Image("Home")
+                    Spacer()
+                    Image("Search")
+                    Spacer()
+                    Image("Reel Outline")
+                    Spacer()
+                    Image("Bag")
+                    Spacer()
+                    Image("Profile")
+        
+                }
+                .padding(.horizontal, 50)
+                .padding()
+                .background(Color("BackgroundColor").ignoresSafeArea())
+            }
         }
     }
 }
